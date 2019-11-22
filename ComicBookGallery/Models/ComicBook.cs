13cd3eb1f522357c67640cc78ba2fb66ac1f656c
Nitesh Parmar <niteshp2007@gmail.com>
2019-11-22 12:39:22 +0000

    Models, and strongly type view

---
+++ ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artists[] Artists { get; set; }
+        public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+
+        // Series-title-issuenumber.jpg e.g
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
+
+    }
+}
