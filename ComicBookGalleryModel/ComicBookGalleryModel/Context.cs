a6b994f8ed01ff90240a8fd96386690bd3b247c1
Nitesh Parmar <niteshp2007@gmail.com>
2020-01-10 14:28:35 +0000

    Updating entity data mode;

---
+++ ComicBookGalleryModel/ComicBookGalleryModel/Context.cs
@@ -0,0 +1,35 @@
+﻿using ComicBookGalleryModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel
+{
+    public class Context : DbContext
+    {
+        //Connection string can be passed to base as well
+        //Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = ComicBookGallery; Integrated Security = True; MultipleActiveResultSets=True
+        public Context() : base("ComicBookGallery")
+        {
+            Database.SetInitializer(new DatabaseIntitalizer());
+        }
+
+        //plural name is common convention
+        public DbSet<ComicBook> ComicBooks { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //modelBuilder.Conventions.Remove<DecimalPropertyConvention>();
+            //modelBuilder.Conventions.Add(new DecimalPropertyConvention(5, 2));
+
+            modelBuilder.Entity<ComicBook>()
+                .Property(cb => cb.AverageRating)
+                .HasPrecision(5,2);
+        }
+
+    }
+}
---
+++ ComicBookGalleryModel/ComicBookGalleryModel/Models/ComicBook.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel.Models
+{
+    public class ComicBook
+    {
+        //defining many to many relationship with artist class
+        public ComicBook()
+        {
+            Artists = new List<ComicBookArtist>();
+        }
+
+        //ID, ComicBookId, ComicBookID example for names used as primary key for EF code first
+        public int Id { get; set; }
+        public int SeriesId { get; set; }
+ //     [ForeignKey("SeriesRefId")]
+        public int IssueNumber { get; set; }
+        public string Description { get; set; }
+        public DateTime PublishedOn { get; set; }
+        public decimal? AverageRating { get; set; }
+
+        public Series Series { get; set; }
+        public ICollection<ComicBookArtist> Artists { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return $"{Series?.Title} #{IssueNumber}";
+            }
+        }
+
+        public void AddArtist(Artist artist, Role role)
+        {
+            Artists.Add(new ComicBookArtist
+            {
+                Artist = artist,
+                Role = role,
+            });
+        }
+    }
+}
